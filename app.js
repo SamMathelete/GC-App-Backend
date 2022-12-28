7825a17e819fb025df930e3af0b71dbc4f4d9a43
LightningItself <68198395+LightningItself@users.noreply.github.com>
2022-12-28 17:10:33 +0530

    created project; added ranking route

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const rankingsRouter = require("./controllers/rankings");
+
+const app = express();
+
+console.log("Connecting to mongoDB server...");
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to mongoDB.");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to mongoDB: ", err.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/rankings", rankingsRouter);
+
+module.exports = app;
---
+++ controllers/rankings.js
@@ -0,0 +1,18 @@
+const rankingsRouter = require("express").Router();
+const Team = require("../models/team");
+
+rankingsRouter.get("/", async (req, res) => {
+  const teams = await Team.find({});
+  const objTeams = teams.map((obj) => obj.toObject());
+  const sortedTeams = teams
+    .map((obj) => obj.toObject())
+    .sort((a, b) => b.score - a.score);
+  const data = sortedTeams.map((obj, index) => {
+    obj.ranking = index + 1;
+    delete obj._id;
+    return obj;
+  });
+  res.json(data);
+});
+
+module.exports = rankingsRouter;
